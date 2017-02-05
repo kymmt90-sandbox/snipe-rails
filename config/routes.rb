afd50044d672368ceabe8f97337376a24d31c769
Kohei Yamamoto <kymmt90@gmail.com>
2017-02-05 22:04:08 +0900

    bin/rails g knock:install && bin/rails g knock:token_controller user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
   resources :users, except: [:index] do
     resources :snippets, shallow: true do
       resources :comments, shallow: true
