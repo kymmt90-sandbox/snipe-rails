6b661cee32d33cb456fb90c9c2a3d859725ce3de
Kohei Yamamoto <kymmt90@gmail.com>
2017-02-05 21:54:54 +0900

    Populate seed data to the development DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
+
+10.times do
+  password = FFaker::Internet.password
+  User.create(
+    name:     FFaker::Name.name,
+    email:    FFaker::Internet.email,
+    password: password,
+    password_confirmation: password
+  )
+end
+
+User.all.each do |user|
+  10.times do
+    user.snippets.create(
+      title: FFaker::Lorem.words.join(' '),
+      content: FFaker::HTMLIpsum.body
+    )
+  end
+end
+
+User.all.each do |user|
+  other = nil
+  loop do
+    other = User.all.to_a.sample
+    break if user != other
+  end
+  other.snippets.to_a.sample(10).each do |snippet|
+    snippet.comments.create(
+      comment_author_id: user.id,
+      content: FFaker::Lorem.paragraph
+    )
+  end
+end
+
+User.all.each do |user|
+  snippets = Snippet.all.to_a.sample(5)
+  snippets.each do |snippet|
+    snippet.stars.create(user_id: user.id)
+  end
+end
