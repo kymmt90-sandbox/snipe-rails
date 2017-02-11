757cdb85f930e27402ebcc6c2aa3ec68800f87d1
Kohei Yamamoto <kymmt90@gmail.com>
2017-02-11 22:35:19 +0900

    Move the Swagger root schema to a concern

--- app/controllers/apidocs_controller.rb
+++ app/controllers/apidocs_controller.rb
@@ -1,30 +1,7 @@
 class ApidocsController < ApplicationController
-  include Swagger::Blocks
-
-  swagger_root do
-    key :swagger, '2.0'
-    info do
-      key :version, '1.0.0'
-      key :title, 'Snipe'
-      key :description, 'A Gist clone'
-      contact do
-        key :name, '@kymmt90'
-      end
-      license do
-        key :name, 'MIT'
-      end
-    end
-    key :basePath, '/api'
-    key :consumes, ['application/json']
-    key :produces, ['application/json']
-  end
-
-  SWAGGERED_CLASSES = [
-    User,
-    self
-  ].freeze
+  include Swagger::RootSchema
 
   def index
-    render json: Swagger::Blocks.build_root_json(SWAGGERED_CLASSES)
+    render json: root_json
   end
 end
