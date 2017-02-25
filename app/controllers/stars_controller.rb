5f87a6fd0a9d452a7e58530160655ae7e9cb8c63
Kōhei Yamamoto <kymmt90@gmail.com>
2017-02-25 17:23:39 +0900

    Star API only returns current user's starring status (#13)

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,10 +1,9 @@
 class StarsController < ApplicationController
-  before_action :set_user
   before_action :set_snippet
   before_action :authenticate_user
 
   def show
-    if @user.starred_snippets.exists?(@snippet.id)
+    if Star.find_by(starring_user: current_user, starred_snippet: @snippet.id).present?
       head :no_content
     else
       head :not_found
@@ -12,24 +11,16 @@ class StarsController < ApplicationController
   end
 
   def create
-    head :unauthorized and return unless current_user == @user
-
-    @user.starred_snippets << @snippet unless @user.starred_snippets.exists?(@snippet.id)
+    current_user.starred_snippets << @snippet unless current_user.starred_snippets.exists?(@snippet.id)
     head :no_content
   end
 
   def destroy
-    head :unauthorized and return unless current_user == @user
-
-    @user.starred_snippets.destroy(@snippet)
+    current_user.starred_snippets.destroy(@snippet)
     head :no_content
   end
 
   private
-
-  def set_user
-    @user = User.find(params[:user_id])
-  end
 
   def set_snippet
     @snippet = Snippet.find(params[:snippet_id])
--- spec/requests/stars_spec.rb
+++ spec/requests/stars_spec.rb
@@ -6,32 +6,33 @@ RSpec.describe 'Star API', type: :request do
 
     let!(:snippet) { create(:snippet) }
 
+    context 'when the authenticated header does not exist' do
+      it 'return 401 Unauthorized' do
+        get "/snippets/#{snippet.id}/star.json"
+        expect(response.status).to eq 401
+      end
+    end
+
     context 'when the specified snippet exists' do
       context 'when the user has already starred the snippet' do
         before { user.starred_snippets << snippet }
 
         it 'returns 204 No Content' do
-          get "/snippets/#{snippet.id}/star.json", params: { user_id: user.id }, headers: authenticated_header
+          get "/snippets/#{snippet.id}/star.json", headers: authenticated_header
           expect(response.status).to eq 204
         end
       end
 
       context 'when the user has not starred the snippet' do
         it 'returns 404 Not Found' do
-          get "/snippets/#{snippet.id}/star.json", params: { user_id: user.id }, headers: authenticated_header
+          get "/snippets/#{snippet.id}/star.json", headers: authenticated_header
           expect(response.status).to eq 404
         end
       end
     end
 
     context 'when the specified snippet does not exist' do
-      before { get "/snippets/#{Snippet.last.id.succ}/star.json", params: { user_id: user.id }, headers: authenticated_header }
-
-      include_examples 'The resource is not found'
-    end
-
-    context 'when the specified user does not exist' do
-      before { get "/snippets/#{snippet.id}/star.json", params: { user_id: User.last.id.succ }, headers: authenticated_header }
+      before { get "/snippets/#{Snippet.last.id.succ}/star.json", headers: authenticated_header }
 
       include_examples 'The resource is not found'
     end
@@ -42,50 +43,41 @@ RSpec.describe 'Star API', type: :request do
 
     let!(:snippet) { create(:snippet) }
 
+    context 'when the authenticated header does not exist' do
+      it 'return 401 Unauthorized' do
+        put "/snippets/#{snippet.id}/star.json"
+        expect(response.status).to eq 401
+      end
+    end
+
     context 'when the specified snippet exists' do
       it 'returns 204 No Content' do
-        put "/snippets/#{snippet.id}/star.json", params: { user_id: user.id }, headers: authenticated_header
+        put "/snippets/#{snippet.id}/star.json", headers: authenticated_header
         expect(response.status).to eq 204
       end
 
       it 'stars the snippet by the user' do
         expect {
-          put "/snippets/#{snippet.id}/star.json", params: { user_id: user.id }, headers: authenticated_header
+          put "/snippets/#{snippet.id}/star.json", headers: authenticated_header
         }.to change(Star, :count).by(1)
       end
 
       it 'binds the user and the snippet' do
-        put "/snippets/#{snippet.id}/star.json", params: { user_id: user.id }, headers: authenticated_header
+        put "/snippets/#{snippet.id}/star.json", headers: authenticated_header
         expect(user.starred_snippets).to eq [snippet]
         expect(snippet.starring_users).to eq [user]
       end
 
       it 'does not star the snippet multiple times by the same user' do
-        put "/snippets/#{snippet.id}/star.json", params: { user_id: user.id }, headers: authenticated_header
+        put "/snippets/#{snippet.id}/star.json", headers: authenticated_header
         expect {
-          put "/snippets/#{snippet.id}/star.json", params: { user_id: user.id }, headers: authenticated_header
+          put "/snippets/#{snippet.id}/star.json", headers: authenticated_header
         }.not_to change(Star, :count)
-      end
-
-      context 'when other user sends the request' do
-        let(:other_user) { create(:user) }
-        let(:authenticated_header) { authentication_token_header(other_user) }
-
-        it 'returns 401 Unauthorized' do
-          put "/snippets/#{snippet.id}/star.json", params: { user_id: user.id }, headers: authenticated_header
-          expect(response.status).to eq 401
-        end
       end
     end
 
     context 'when the specified snippet does not exist' do
-      before { get "/snippets/#{Snippet.last.id.succ}/star.json", params: { user_id: user.id }, headers: authenticated_header }
-
-      include_examples 'The resource is not found'
-    end
-
-    context 'when the specified user does not exist' do
-      before { get "/snippets/#{snippet.id}/star.json", params: { user_id: User.last.id.succ }, headers: authenticated_header }
+      before { get "/snippets/#{Snippet.last.id.succ}/star.json", headers: authenticated_header }
 
       include_examples 'The resource is not found'
     end
@@ -96,41 +88,26 @@ RSpec.describe 'Star API', type: :request do
 
     let!(:snippet) { create(:snippet) }
 
+    context 'when the authenticated header does not exist' do
+      it 'return 401 Unauthorized' do
+        delete "/snippets/#{snippet.id}/star.json"
+        expect(response.status).to eq 401
+      end
+    end
+
     context 'when the specified snippet exists' do
       before { user.starred_snippets << snippet }
 
       it 'returns 204 No Content' do
-        delete "/snippets/#{snippet.id}/star.json", params: { user_id: user.id }, headers: authenticated_header
+        delete "/snippets/#{snippet.id}/star.json", headers: authenticated_header
         expect(response.status).to eq 204
       end
 
       it 'unstars the snippet by the user' do
         expect {
-          delete "/snippets/#{snippet.id}/star.json", params: { user_id: user.id }, headers: authenticated_header
+          delete "/snippets/#{snippet.id}/star.json", headers: authenticated_header
         }.to change(Star, :count).by(-1)
       end
-
-      context 'when other user sends the request' do
-        let(:other_user) { create(:user) }
-        let(:authenticated_header) { authentication_token_header(other_user) }
-
-        it 'returns 401 Unauthorized' do
-          delete "/snippets/#{snippet.id}/star.json", params: { user_id: user.id }, headers: authenticated_header
-          expect(response.status).to eq 401
-        end
-      end
-    end
-
-    context 'when the specified snippet does not exist' do
-      before { get "/snippets/#{Snippet.last.id.succ}/star.json", params: { user_id: user.id }, headers: authenticated_header }
-
-      include_examples 'The resource is not found'
-    end
-
-    context 'when the specified user does not exist' do
-      before { get "/snippets/#{snippet.id}/star.json", params: { user_id: User.last.id.succ }, headers: authenticated_header }
-
-      include_examples 'The resource is not found'
     end
   end
 end
