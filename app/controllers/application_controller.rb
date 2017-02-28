45f193e327ac6e5d1db0b8392185e84146fb327f
Kohei Yamamoto <kymmt90@gmail.com>
2017-02-28 23:08:43 +0900

    404 error responses have an array of error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::API
   include Knock::Authenticable
 
   rescue_from ActiveRecord::RecordNotFound do |e|
-    render json: { error: e.message }, status: :not_found
+    response = {
+      errors: [
+        "#{e.model} not found"
+      ]
+    }
+    render json: response, status: :not_found
   end
 
   rescue_from ActionController::ParameterMissing do |e|
