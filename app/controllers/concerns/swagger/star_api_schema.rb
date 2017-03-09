7264a21c0cd40ff66ecd46b665ad47b587d7a28b
Kōhei Yamamoto <kymmt90@gmail.com>
2017-03-09 23:22:50 +0900

    Write star API errors schema in OpenAPI (#25)

--- app/controllers/concerns/swagger/star_api_schema.rb
+++ app/controllers/concerns/swagger/star_api_schema.rb
@@ -16,25 +16,80 @@ module Swagger::StarApiSchema
 
       operation :get do
         key :description, 'Check the snippet is starred'
+
         response 204 do
           key :description, 'The snippet is starred when the API returns no content'
         end
+
+        response 401 do
+          key :description, 'unauthorized user'
+          schema do
+            key :'$ref', :ErrorOutput
+          end
+        end
+
+        response 404 do
+          key :description, 'The snippet is **not** starred when the API returns no content / snippet not found'
+          schema do
+            key :'$ref', :ErrorOutput
+          end
+        end
+
+        response :default do
+          key :description, 'unexpected error'
+          schema do
+            key :'$ref', :ErrorOutput
+          end
+        end
+
         security api_token: []
       end
 
       operation :put do
         key :description, 'Star the snippet'
+
         response 204 do
           key :description, 'The snippet has been starred when the API returns no content'
         end
+
+        response 401 do
+          key :description, 'unauthorized user'
+          schema do
+            key :'$ref', :ErrorOutput
+          end
+        end
+
+        response :default do
+          key :description, 'unexpected error'
+          schema do
+            key :'$ref', :ErrorOutput
+          end
+        end
+
         security api_token: []
       end
 
       operation :delete do
         key :description, 'Unstar the snippet'
+
         response 204 do
           key :description, 'The snippet has been unstarred when the API returns no content'
         end
+
+        response 401 do
+          key :description, 'unauthorized user'
+          schema do
+            key :'$ref', :ErrorOutput
+          end
+        end
+
+        response :default do
+          key :description, 'unexpected error'
+          schema do
+            key :'$ref', :ErrorOutput
+          end
+        end
+
         security api_token: []
       end
     end
