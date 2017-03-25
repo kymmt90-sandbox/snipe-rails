7194196f93b99d8a70a665a8e019588f3c2d30cb
Kohei Yamamoto <kymmt90@gmail.com>
2017-03-25 18:39:01 +0900

    Indicate links to other page resources with the link header field

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -10,8 +10,12 @@ class SnippetsController < ApplicationController
     relation = Snippet.includes(:author)
     if params[:user_id].present?
       author = User.find(params[:user_id])
-      relation = relation.where(author_id: author)
+      relation = relation.where(author_id: author.id)
+      response.headers['Link'] = link_header(url_for(:user_snippets), author.id)
+    else
+      response.headers['Link'] = link_header(url_for(:snippets))
     end
+
     @snippets = relation.limit(INDEX_MAX_SNIPPETS).page(params[:page])
   end
 
@@ -55,5 +59,33 @@ class SnippetsController < ApplicationController
 
   def snippet_params
     params.require(:snippet).permit(:title, :content)
+  end
+
+  def link_header(url, author_id = nil)
+    @current_page = Snippet.page(params[:page]).current_page
+    @page_out_of_range = Snippet.page(params[:page]).out_of_range?
+    @total_pages = Snippet.total_pages(author_id)
+
+    [first_link(url), previous_link(url), next_link(url), last_link(url)].compact.join(', ')
+  end
+
+  def first_link(url)
+    %(<#{url}?page=1>; rel="first")
+  end
+
+  def previous_link(url)
+    return if @page_out_of_range || @current_page <= 1
+
+    %(<#{url}?page=#{@current_page - 1}>; rel="previous")
+  end
+
+  def next_link(url)
+    return if @page_out_of_range || @current_page >= @total_pages
+
+    %(<#{url}?page=#{@current_page + 1}>; rel="next")
+  end
+
+  def last_link(url)
+    %(<#{url}?page=#{@total_pages}>; rel="last")
   end
 end
--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -8,4 +8,12 @@ class Snippet < ApplicationRecord
   has_many :comments
   has_many :stars, foreign_key: 'snippet_id'
   has_many :starring_users, through: :stars, class_name: 'User'
+
+  def self.total_pages(author_id = nil)
+    if author_id
+      where(author_id: author_id).total_pages
+    else
+      page(1).total_pages
+    end
+  end
 end
