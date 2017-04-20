74eda91b365367a1487e72301de14249c9c0bc5d
Kōhei Yamamoto <kymmt90@gmail.com>
2017-04-20 23:36:30 +0900

    Accept clients to use the Link header (#34)

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module SnipeRails
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options], expose: ['Link']
       end
     end
   end
